164b2301cf69801e864c0c07d3d5b2d5993ace08
Echizen <harry.chiu.hc@gmail.com>
2024-10-24 15:46:59 +0100

    Added XR locomotion functionalities

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,24 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.InputSystem;
 using TMPro;
+using UnityEngine;
+//using UnityEngine.InputSystem;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 0;
+    public float speed = 0.1f;
     public TextMeshProUGUI countText;
     public GameObject winTextObject;
     public GameObject enemyObject;
 
     private Rigidbody rb;
     private int count;
-    private float movementX;
-    private float movementY;
+    //private float movementX;
+    //private float movementY;
+    //public XRController rightHand;
+    public XRBaseController rightHandController; // Use Right Hand controller
+    private Vector3 movementDirection;
 
     // Start is called before the first frame update
     void Start()
@@ -31,14 +36,50 @@ public class PlayerController : MonoBehaviour
         // Debug.Log("Hello!");  
     }
 
-    void OnMove(InputValue movementValue)
-    {
-        Vector2 movementVector = movementValue.Get<Vector2>();
+    //void OnMove(InputValue movementValue)
+    //{
+    //    Vector2 movementVector = movementValue.Get<Vector2>();
 
-        movementX = movementVector.x;
-        movementY = movementVector.y;
+    //    movementX = movementVector.x;
+    //    movementY = movementVector.y;
+    //}
+    void Update()
+    {
+        // Get the controller's rotation
+        //if (rightHandController != null && rightHandController.inputDevice.isValid)
+        //{
+        //    Quaternion controllerRotation = rightHandController.inputDevice.TryGetFeatureValue(CommonUsages.deviceRotation, out var rotation) ? rotation : Quaternion.identity;
+        //    movementDirection = controllerRotation * Vector3.forward;
+        //}
+        //if (rightHandController != null)
+        //{
+        //    // Get the rotation of the controller
+        //    Quaternion controllerRotation = rightHandController.transform.rotation;
+
+        //    Vector3 forwardDirection = controllerRotation * Vector3.forward;
+
+        //    // Get the tilt angle (pitch) of the controller
+        //    float pitchAngle = Vector3.SignedAngle(Vector3.up, forwardDirection, rightHandController.transform.right);
+
+        //    // Adjust the movement direction based on pitch angle
+        //    if (pitchAngle > 5f)  // Tilt up threshold
+        //    {
+        //        // If tilted up beyond 5 degrees, move backward
+        //        movementDirection = -forwardDirection;
+        //    }
+        //    else if (pitchAngle < -5f)  // Tilt down threshold
+        //    {
+        //        // If tilted down beyond -5 degrees, move forward
+        //        movementDirection = forwardDirection;
+        //    }
+        //    else
+        //    {
+        //        // If within the small angle range, don't move
+        //        movementDirection = Vector3.zero;
+        //    }
+        //}
     }
-    
+
     void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
@@ -50,7 +91,7 @@ public class PlayerController : MonoBehaviour
     }
     void FixedUpdate()
     {   
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        Vector3 movement = new Vector3(movementDirection.x, 0.0f, movementDirection.z);
 
         rb.AddForce(movement * speed);
     }
